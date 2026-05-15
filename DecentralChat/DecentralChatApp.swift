7e0b3d63ad4b83a86ac484cae0733c00943e5700
Joseph Lee <markcat@live.com>
2026-05-15 21:49:55 +0900

    Initial Commit

---
+++ DecentralChat/DecentralChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecentralChatApp.swift
+//  DecentralChat
+//
+//  Created by Joseph Lee on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct DecentralChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
